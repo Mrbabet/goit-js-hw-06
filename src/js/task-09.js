39e9627c73ea1712300dbadec9a6a6b56122be10
Mrbabet <108229726+Mrbabet@users.noreply.github.com>
2023-09-29 11:31:52 +0200

    Add files via upload

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorSpan = document.querySelector(".color");
+const colorButton = document.querySelector(".change-color");
+
+const changeColor = () => {
+  const color = getRandomHexColor();
+  colorSpan.textContent = color;
+  document.querySelector("body").style.backgroundColor = color;
+};
+colorButton.addEventListener("click", changeColor);
